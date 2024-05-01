cc73c89b42860633fcb2b6a6ef2f7963d266613c
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-05-01 01:42:36 +0000

    task 102

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -25,7 +25,6 @@ void swap_ints(int *a, int *b)
  * @array: An array of integers.
  * @size: The size of the array.
  * @start: The starting index of the sequence in array to sort.
- 
  * @seq: The size of the sequence to sort.
  * @flow: The direction to sort in.
  */
@@ -34,7 +33,7 @@ void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
 {
 	size_t i, jump = seq / 2;
 
-	if(seq > 1)
+	if (seq > 1)
 	{
 		for (i = start; i < start + jump; i++)
 		{
